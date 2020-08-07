3078505569327440c6f9abe25484a96d7077e5ec
Amir-16 <greenfitter516@gmail.com>
2020-08-07 12:48:36 +0600

    second commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About us</title>
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+    <div>
+
+        <div class="page-heading">
+            <h2>About us</h2>
+
+        </div>
+
+        <div class="content">
+
+            <p>This is about us page</p>
+        <p>Name is:{{$myname}}</p>
+        <p>query value is:{{$q}}</p>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('about/{v}', function ($vq) {
+    $name='Amir';
+    return view('about',['myname'=>$name,'q'=>$vq]); //key=>value pair
 });
